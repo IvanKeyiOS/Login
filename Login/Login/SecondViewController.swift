3836e29dc3067acc51c71461a44bdc22946c29e4
IvanKeyiOS <ivanbeat888@gmail.com>
2025-01-06 03:10:32 +0100

    first lesson with Darya

---
+++ Login/Login/SecondViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SecondViewController.swift
+//  Login
+//
+//  Created by Иван Курганский on 04/01/2025.
+//
+
+import UIKit
+
+final class SecondViewController: UIViewController {
+    
+}
---
+++ Login/Login/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Иван Курганский on 04/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //MARK: - Life cycle UI
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
+    

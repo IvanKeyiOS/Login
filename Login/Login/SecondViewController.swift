56e4a3d7404c405db88d839c15a9b99b4fcc92c0
IvanKeyiOS <ivanbeat888@gmail.com>
2025-01-07 20:21:34 +0100

    add HW 2.4

---
+++ Login/Login/SecondViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SecondViewController.swift
+//  Login
+//
+//  Created by Иван Курганский on 07/01/2025.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+}
--- Login/Login/ViewController.swift
+++ Login/Login/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var lineUnderEmailView: UIView!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var lineUnderPasswordView: UIView!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signHereButton: UIButton!
+    @IBOutlet weak var doNotHaveAccountLabel: UILabel!
+    
+    // MARK: - Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setButtonConfiguration()
     }
-
-
+    
+    private func setButtonConfiguration() {
+        loginButton.layer.cornerRadius = 6
+        loginButton.layer.shadowColor = UIColor.systemBlue.cgColor
+        loginButton.layer.shadowOpacity = 0.5
+        loginButton.layer.shadowOffset = CGSize(width: 0, height: 3)
+    }
+    
+    //MARK: - IBActions
+    @IBAction func loginButtonAction(_ sender: Any) {
+        print("login button was push")
+    }
+    
+    @IBAction func signHereButtonAction(_ sender: Any) {
+        print("sign here button was push")
+    }
+    
 }
 
